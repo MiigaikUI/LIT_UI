1c21badce8015a8ac9f227063a2bee97a8e46fbe
I-SMAF <ikarashov@gmail.com>
2022-03-02 12:49:14 +0300

    Update views.py

--- Clock/views.py
+++ Clock/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 
-from LIT_UI.settings import ALLOWED_HOSTS
+from LIT_UI.settings import ALLOWED_HOSTS, get_ip_address
 
 
 class ClockView(View):
     def get(self, request):
-        return render(request, "Clock.html", {"IP", ALLOWED_HOSTS[0]})
+        print(ALLOWED_HOSTS[0])
+        return render(request, "Clock.html", {"IP", get_ip_address()})
