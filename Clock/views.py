006bdbd0abdd3a55298df161ab0beacf0b4c8d62
I-SMAF <ikarashov@gmail.com>
2022-03-02 12:47:27 +0300

    idk2

--- Clock/views.py
+++ Clock/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
+from LIT_UI.settings import ALLOWED_HOSTS
+
 
 class ClockView(View):
     def get(self, request):
-        return render(request, "Clock.html")
+        return render(request, "Clock.html", {"IP", ALLOWED_HOSTS[0]})
